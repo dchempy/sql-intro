a0a93e94c5ced216bbd1d186c256310a7af1d820
Dhruv Chempakasseril <dhruv.chempakasseril@kellogg.northwestern.edu>
2024-01-11 01:25:14 +0000

    completed first portion of SQL excersizes

--- demo.sql
+++ demo.sql
@@ -1,3 +1,12 @@
 -- Get the first and last name of every player in the database.
 
 SELECT first_name, last_name FROM players;
+
+-- sqlite3 baseball.sqlite3
+-- .schema -> shows tables
+-- .mode columns -> adds columns to terminal output
+-- .headers on --> adds headers to terminal output
+-- .read _____ --> how to run sql code
+-- .quit --> how to quit sqlite3
+-- ; --> add if you get ...> to break command
+-- CTRL L --> hit keys to clear terminal
